0ddae51f18f7d2cb0dcf9a2454aa923a34475275
Neranjaka Jayarathne <neranjaka@yahoo.com>
2019-09-21 21:58:11 -0500

    First Graph

--- bcs.m
+++ bcs.m
@@ -1,13 +1,17 @@
 function res = bcs(za,zb)
 
-res = [ za(1)-pi/3 
+res = [ za(1) 
     zb(1)-pi/10 
     za(2) 
     zb(2)
-    za(3)-pi/4 
-    zb(3)-pi/10
+    za(3)
+    zb(3)
     za(4) 
-    zb(4) ];
+    zb(4)
+    za(5)
+    zb(5)
+    za(6)
+    zb(6)];
 
 end
 
--- simpleBVP.m
+++ simpleBVP.m
@@ -1,22 +1,28 @@
 % function simpleBVP()
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
-solinit = bvpinit([0,1],[pi/3,0,pi/4,0,1,0,1,0]);
-sol = bvp4c(@derive,@bcs,solinit);
+clear
+clc
+solinit = bvpinit([0,1],[pi/10,0,0,0,0,0,0,1,0,1,0,1]);
+
+options = bvpset('NMax',1500);
+sol = bvp4c(@derive,@bcs,solinit,options);
 %sol.x
 %sol.y(1,:)
 
-plot(sol.y(1,:),sol.y(3,:),'b-x');
+%plot(sol.y(1,:),sol.y(3,:),'b-x');
+plot(sol.x,sol.y(1,:),'b-x');
 
-figure
+
+%figure
 
 
-subplot(2,2,2)
-plot(sol.x,sol.y(2,:),'b-x'); hold on; plot(sol.x,sol.y(4,:),'r-x');
+%subplot(2,2,2)
+%plot(sol.x,sol.y(2,:),'b-x'); hold on; plot(sol.x,sol.y(4,:),'r-x');
 
-subplot(2,2,3)
-plot(sol.x,sol.y(1,:),'b-x'); hold on; plot(sol.x,sol.y(3,:),'r-x');
+%subplot(2,2,3)
+%plot(sol.x,sol.y(1,:),'b-x'); hold on; plot(sol.x,sol.y(3,:),'r-x');
 % end
 
-subplot(2,2,4)
-plot(sol.x,sol.y(6,:)./sin(sol.y(3,:)/2).^2,'b-x'); hold on; plot(sol.x,4*sol.y(8,:),'r-x');
+%subplot(2,2,4)
+%plot(sol.x,sol.y(6,:)./sin(sol.y(3,:)/2).^2,'b-x'); hold on; plot(sol.x,4*sol.y(8,:),'r-x');
